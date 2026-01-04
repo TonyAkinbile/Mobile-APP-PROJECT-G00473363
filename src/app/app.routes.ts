3f6443af94ef3ccc1f1e4014f7168b66fa8024d6
Your Name <tonyakinbile92@gmail.com>
2026-01-04 02:44:15 +0000

    Add Home Page UI, routing fixes, and search logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,28 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'recipe-details',
-    loadComponent: () => import('./pages/recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
+    loadComponent: () =>
+      import('./pages/recipe-details/recipe-details.page').then((m) => m.RecipeDetailsPage),
   },
   {
     path: 'favourites',
-    loadComponent: () => import('./pages/favourites/favourites.page').then( m => m.FavouritesPage)
+    loadComponent: () =>
+      import('./pages/favourites/favourites.page').then((m) => m.FavouritesPage),
   },
   {
     path: 'settings',
-    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+    loadComponent: () =>
+      import('./pages/settings/settings.page').then((m) => m.SettingsPage),
   },
 ];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,49 @@
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButtons, IonButton, IonIcon, IonItem, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonItem,
+    IonInput,
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
+
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  ingredients: string = "";
+
+
+  constructor(private router: Router) { }
+
 
   ngOnInit() {
   }
+  searchRecipes() {
+    this.router.navigate(['/recipe-details'], {
+      queryParams: { ingredients: this.ingredients }
+    });
+  }
+
+
+
 
 }
