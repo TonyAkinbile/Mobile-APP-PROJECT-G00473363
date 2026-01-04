ccb42e923fc0b808231c662d21808e868f3e375e
Your Name <tonyakinbile92@gmail.com>
2026-01-04 16:38:04 +0000

    Add favourites removal logic and UI update

--- src/app/pages/favourites/favourites.page.ts
+++ src/app/pages/favourites/favourites.page.ts
@@ -1,3 +1,4 @@
+import { FavouritesService } from '../../services/favourites.service';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +12,17 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class FavouritesPage implements OnInit {
+  favourites: any[] = [];
 
-  constructor() { }
+
+  constructor(private favouritesService: FavouritesService) { }
 
   ngOnInit() {
+    this.favourites = this.favouritesService.getFavourites();
+  }
+  removeFavourite(id: number) {
+    this.favouritesService.removeFavourite(id);
+    this.favourites = this.favouritesService.getFavourites();
   }
 
 }
